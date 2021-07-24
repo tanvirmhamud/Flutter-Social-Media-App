85e6f950812bee7693478bf56bc89fd5ea3d584c
tanvirmhamud <shakilhassan887@gmail.com>
2021-07-24 09:46:55 +0600

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,13 @@ import 'package:flutter_social_media/Account/login.dart';
 import 'package:flutter_social_media/Provider/firebasedata.dart';
 import 'package:flutter_social_media/Provider/account.dart';
 import 'package:flutter_social_media/Provider/otherfunction.dart';
+import 'package:flutter_social_media/Provider/userprofilefunction.dart';
 import 'package:flutter_social_media/homepage.dart';
-
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-
+import 'Provider/commentfunction.dart';
 import 'Provider/imageupload.dart';
+import 'Provider/likefunction.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -18,25 +19,18 @@ void main() async {
   var finalemail = sharedPreferences.getString('email');
   runApp(MultiProvider(
       providers: [
-        ChangeNotifierProvider(
-          create: (context) => OthderFunctionprovider(),
-        ),
-        ChangeNotifierProvider(
-          create: (context) => FirebasedataProvider(),
-        ),
-        ChangeNotifierProvider(
-          create: (context) => ImageUploadProvider(),
-        ),
-        ChangeNotifierProvider(
-          create: (context) => Accountprovider(),
-        )
+        ChangeNotifierProvider(create: (context) => UserProfileProvider()),
+        ChangeNotifierProvider(create: (context) => CommentFunction()),
+        ChangeNotifierProvider(create: (context) => Likeprovider()),
+        ChangeNotifierProvider(create: (context) => OthderFunctionprovider()),
+        ChangeNotifierProvider(create: (context) => FirebasedataProvider()),
+        ChangeNotifierProvider(create: (context) => ImageUploadProvider()),
+        ChangeNotifierProvider(create: (context) => Accountprovider())
       ],
       child: (MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
+        theme: ThemeData(primarySwatch: Colors.blue),
         home: finalemail == null ? LoginPage() : Homepage(),
       ))));
 }
